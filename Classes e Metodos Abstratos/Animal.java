dc9bba9679ffebedeb5b869d2e75b0517b03e156
Emanuel <108434968+EmanuelDeBrito@users.noreply.github.com>
2025-09-16 14:59:07 -0300

    Learning About Abstract Class and Abstract Method

---
+++ Classes e Metodos Abstratos/Animal.java
@@ -0,0 +1,11 @@
+package OO;
+
+// Classe Abstrata
+public abstract class Animal {
+    public void respirar(){
+        System.out.println("Respirando...");
+    }
+
+    // Método Abstrato
+    public abstract void fazerBarulho();
+}
